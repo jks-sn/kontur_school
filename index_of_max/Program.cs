d62b472ad220ee41225195a177f9aad14b023915
Jhon <maxotr@mail.ru>
2024-10-12 21:11:17 +0700

    add index_of_max

---
+++ index_of_max/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace index_of_max;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine($"Index of max: {MaxIndex(array)}.");
+    }
+    
+    public static int MaxIndex(double[] array)
+    {
+        return array.Length == 0 ? -1 : Array.IndexOf(array, array.Max());
+    }
+
+}
