48c789cce03b37ff07b0550e4a87f47b308645da
Jhon <maxotr@mail.ru>
2024-10-12 20:48:36 +0700

    add even_massives solution, work

--- even_massives/Program.cs
+++ even_massives/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Program.cs
 
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Пример вызова метода GetFirstEvenNumbers
+        int[] result = GetFirstEvenNumbers(4);
+
+        // Вывод результата на экран
+        Console.WriteLine("Четные числа: " + string.Join(", ", result));
+    }
+
+    public static int[] GetFirstEvenNumbers(int count)
+    {
+        int[] evenNumbers = new int[count];
+        
+        for (int i = 0; i < count; ++i)
+        {
+            evenNumbers[i] = (i + 1) * 2;
+        }
+        
+        return evenNumbers;
+    }
+}
