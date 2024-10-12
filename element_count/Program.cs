47f86bbf7ee45ad4e1766adadea87f3718039eb6
Jhon <maxotr@mail.ru>
2024-10-12 22:40:06 +0700

    new task

---
+++ element_count/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace elementCount;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 1, 1 };
+        Console.WriteLine($"Число 1 встречается {GetElementCount(array, 1)} раз(а)"); // Выведет 3
+    }
+    public static int GetElementCount(int[] items, int itemToCount)
+    {
+        return items.Count(item => item == itemToCount);
+    }
+}
