42b83962be70ae8c853f38d9ba83561851afa357
Jhon <maxotr@mail.ru>
2024-10-13 21:40:37 +0700

    add taro task

---
+++ taro_cards/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace taro_cards;
+
+class Program
+{
+    enum Suits
+    {
+        Wands,
+        Coins,
+        Cups,
+        Swords
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetSuit(Suits.Wands));
+        Console.WriteLine(GetSuit(Suits.Coins));
+        Console.WriteLine(GetSuit(Suits.Cups));
+        Console.WriteLine(GetSuit(Suits.Swords));
+    }
+    private static string GetSuit(Suits suit)
+    {
+        return suit switch
+        {
+            Suits.Wands => "жезлов",
+            Suits.Coins => "монет",
+            Suits.Cups => "кубков",
+            Suits.Swords => "мечей"
+        };
+    }
+}
