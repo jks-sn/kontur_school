7d4d982cd449225b53bedae7584b922c53e6186d
Jhon <maxotr@mail.ru>
2024-10-14 16:28:22 +0700

    add histogram tasks

---
+++ histogram/HeatmapTask.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Names;
+internal static class HeatmapTask
+{
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    {
+        var days = Enumerable.Range(2, 30).Select(day => day.ToString()).ToArray(); 
+        var months = Enumerable.Range(1, 12).Select(month => month.ToString()).ToArray(); 
+        var birthCounts = new double[30, 12];
+        foreach (var person in names.Where(person => person.BirthDate.Day > 1))
+        {
+            birthCounts[person.BirthDate.Day - 2, person.BirthDate.Month - 1]++;
+        }
+        return new HeatmapData("Карта интенсивности рождаемости в зависимости от дня и месяца",  birthCounts, days, months);
+    }
+}
---
+++ histogram/HistogramTask.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var days = Enumerable.Range(1, 31).Select(day => day.ToString()).ToArray();
+        
+        var birthCounts = new double[31];
+
+        foreach (var person in names.Where(person => person.Name == name && person.BirthDate.Day != 1))
+        {
+            birthCounts[person.BirthDate.Day - 1]++;
+        }
+        return new HistogramData($"Рождаемость людей с именем '{name}'", days, birthCounts);
+    }
+}
